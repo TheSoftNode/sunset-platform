38fe12d3a566ea4c34cf99997bf946b95b4770d8
TheSoftNode <thesoftnode@gmail.com>
2024-10-13 11:23:20 +0100

    Add the hanging button

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Routers from "../Routes/Routers";
+import Hangging_btn from "../components/General/Hangging_btn";
 
 
 
@@ -15,6 +16,7 @@ const Layout = () =>
         <Routers />
       </main>
       <Footer />
+      <Hangging_btn />
     </>
   );
 };
