1df4b4b584aab3572831288bd412525d7bc131f5
TheSoftNode <thesoftnode@gmail.com>
2024-10-13 22:50:26 +0100

    Register form errors

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -35,7 +35,7 @@ const Signup = () =>
 
                 <p className='mt-4 text-center text-gray-600'>
                     Already have an account?{' '}
-                    <Link to='/login' className='text-blue-500 font-semibold hover:underline'>
+                    <Link to='/login'>
                         Log In
                     </Link>
                 </p>
